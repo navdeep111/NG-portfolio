a0aba04e04220279474534bca911c7695f041ccc
navdeep111 <guptanavdeep9461@gmail.com>
2025-02-27 23:42:36 +0530

    chnages and SEO

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -5,21 +5,7 @@ import {CgFileDocument} from "react-icons/cg";
 
 const  ProjectBox = ({projectPhoto, projectName}) => {
   const desc = {
-    TindogDesc : "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub : "https://github.com/DevanshSahni/tindog",
-    TindogWebsite : "https://devanshsahni.github.io/tindog/",
-
-    RogFreeDesc : "A website that shows you over seven specialized yoga postures for specific diseases or health problems. This was a group project made in a team of two for a 36-hour-long online hackathon named Hackodisha 2.0.",
-    RogFreeGithub : "https://github.com/DevanshSahni/Rog-Free",
-    RogFreeWebsite : "https://devanshsahni.github.io/Rog-Free/",
-
-    NewsletterDesc:"A newsletter signup site made using Mailchimp API where the signups can be monitored from the MailChimp account. This project was made to understand API integration, environment variables and vercel deployment.",
-    NewsletterGithub:"",
-    NewsletterWebsite:"https://newsletter-signup-teal.vercel.app/",
     
-    WigglesDesc:"An innovative pet management web app enabling pet parents to create unique pet IDs, securely store and share vaccination records, and generate QR codes for pet profiles, enhancing safety.",
-    WigglesGithub:"https://github.com/DevanshSahni/Wiggles",
-    WigglesWebsite:"https://wiggles.vercel.app/",
 
     QuotesDesc:"Random Quotes Generator is a simple web application that displays random inspirational, motivational, or humorous quotes at the click of a button. It helps users find daily inspiration and share meaningful words effortlessly.",
     QuotesGithub:"https://github.com/navdeep111/Random-Quotes-Generator-",
@@ -28,6 +14,8 @@ const  ProjectBox = ({projectPhoto, projectName}) => {
     MomomiaDesc:"Food Ordering Website is a user-friendly platform that allows customers to browse menus, place orders.",
     MomomiaGithub:"https://github.com/navdeep111/food-ordering-website",
     MomomiaWebsite:"https://momomia.navdeep.tech/",
+
+
   }
 
   let show ='';
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ProjectBox from './ProjectBox';
-import NewsletterImage from '../images/NewsletterImage.png';
-import RogfreeImage from '../images/RogfreeImage.png';
-import TindogImage from '../images/TindogImage.png';
-import WigglesImage from '../images/WigglesImage.png';
+
 import QuotesImage from '../images/QuotesImage.png';
 import MomomiaImage from '../images/MomomiaImage.png';
 
@@ -14,10 +11,7 @@ const Projects = () => {
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={WigglesImage} projectName="Wiggles" />
-        <ProjectBox projectPhoto={NewsletterImage} projectName="Newsletter" />
-        <ProjectBox projectPhoto={RogfreeImage} projectName="RogFree" />
-        <ProjectBox projectPhoto={TindogImage} projectName="Tindog" />
+        
         <ProjectBox projectPhoto={QuotesImage} projectName="Quotes" />
         <ProjectBox projectPhoto={MomomiaImage} projectName="Momomia" />
 
